87845d5f8260e6da796dfcd9115a1bea69980fe5
Kenneth Tso <iamkennytso@gmail.com>
2017-11-06 14:00:32 -0500

    (feat) place method works

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-var board = [[1,2,3],[4,5,6],[7,8,9]]
-var board2 = [[1,'x',3],[4,'o',6],[7,8,9]]
-
+var board = [[0,1,2],[3,4,5],[6,7,8]]
+var turn = true
 const printBoard = (board) => {
   for ( let i = 0; i < board.length; i++){
     var print = ''
@@ -13,13 +12,22 @@ const printBoard = (board) => {
   }
 }
 console.log('Welcome to 1 hour time limit tic tac toe!')
-console.log('left arrow means it is X turn')
-console.log('right arrow means it is O turn')
 console.log('to place your piece somewhere, simply enter in "place(x)", ')
-console.log('where x is the position on the board. here are the positions: ')
+console.log('where x is the number position on the board. here are the positions: ')
 console.log(board[0])
 console.log(board[1])
 console.log(board[2])
 console.log('currently your board looks like this: ')
 printBoard(board)
-console.log('Have fun!')
+console.log('X goes first, have fun!')
+const place = (x) => {
+  var i = Math.floor(x / 3)
+  var j = x % 3
+  if (turn) board[i][j] = 'X'
+  else board[i][j] = 'O'
+  turn = !turn
+  printBoard(board)
+  if(!turn) console.log(`Now it's O's turn`)
+  else console.log(`Now it's X's turn`)
+}
+
