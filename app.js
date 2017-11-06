5748378c5b130d9fff769553112d3f9f4878fd6f
Kenneth Tso <iamkennytso@gmail.com>
2017-11-06 14:16:20 -0500

    (feat) ugly win check

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ const printBoard = (board) => {
     if(i !== 2) console.log('______')
   }
 }
+const clearBoard = (board, turn) => {
+  console.log('Board Clearing...')
+  board = [[0,1,2],[3,4,5],[6,7,8]]
+  turn = true
+}
+const place = (x) => {
+  var i = Math.floor(x / 3)
+  var j = x % 3
+  if (turn) board[i][j] = 10
+  else board[i][j] = 100
+  turn = !turn
+  printBoard(board)
+  checkWin(board)
+  if(!turn) console.log(`Now it's O's turn`)
+  else console.log(`Now it's X's turn`)
+}
 console.log('Welcome to 1 hour time limit tic tac toe!')
 console.log('to place your piece somewhere, simply enter in "place(x)", ')
 console.log('where x is the number position on the board. here are the positions: ')
@@ -23,20 +39,82 @@ console.log(board[2])
 console.log('currently your board looks like this: ')
 printBoard(board)
 console.log('X goes first, have fun!')
-const place = (x) => {
-  var i = Math.floor(x / 3)
-  var j = x % 3
-  if (turn) board[i][j] = 10
-  else board[i][j] = 100
-  turn = !turn
-  printBoard(board)
-  if(!turn) console.log(`Now it's O's turn`)
-  else console.log(`Now it's X's turn`)
+
+const checkWin = (arr) => {
+  if(arr[0][0]+arr[0][1]+arr[0][2] === 30) {
+    console.log(`X wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[1][0]+arr[1][1]+arr[1][2] === 30) {
+    console.log(`X wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[2][0]+arr[2][1]+arr[2][2] === 30) {
+    console.log(`X wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[0][0]+arr[1][0]+arr[1][0] === 30) {
+    console.log(`X wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[0][1]+arr[1][1]+arr[2][1] === 30) {
+    console.log(`X wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[0][2]+arr[0][2]+arr[0][2] === 30) {
+    console.log(`X wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[0][0]+arr[1][1]+arr[2][2] === 30) {
+    console.log(`X wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[0][2]+arr[1][1]+arr[2][0] === 30) {
+    console.log(`X wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+  //
+    if(arr[0][0]+arr[0][1]+arr[0][2] === 300) {
+    console.log(`O wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[1][0]+arr[1][1]+arr[1][2] === 300) {
+    console.log(`O wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[2][0]+arr[2][1]+arr[2][2] === 300) {
+    console.log(`O wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[0][0]+arr[1][0]+arr[1][0] === 300) {
+    console.log(`O wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[0][1]+arr[1][1]+arr[2][1] === 300) {
+    console.log(`O wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[0][2]+arr[0][2]+arr[0][2] === 300) {
+    console.log(`O wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[0][0]+arr[1][1]+arr[2][2] === 300) {
+    console.log(`O wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
+    if(arr[0][2]+arr[1][1]+arr[2][0] === 300) {
+    console.log(`O wins! Clearing Board for new game`)
+    clearBoard(board)
+  }
 }
 
-const checkWin = () => {
-  
-}
+
 
 place(4)
 place(0)
+place(1)
+clearBoard(board)
+console.log(board)
+place(4)
+
+
